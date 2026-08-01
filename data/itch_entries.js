8e93a7b10fb25822e66c1170bbe283010bb44923
bbbbbr <bbbbbr@users.noreply.github.com>
2026-08-01 09:54:15 -0700

    Entries: RetroChron is now a free download

--- data/itch_entries.js
+++ data/itch_entries.js
@@ -6182,7 +6182,7 @@ const jsonGalleryItemsItch = [
     "isMultiPlatform": false,
     "isOpenSource": false,
     "licenseType": "",
-    "isFreeDownload": false,
+    "isFreeDownload": true,
     "hasPhysicalRelease": false,
     "isClassicGBDK": false,
     "usesEngine": "No Engine",
